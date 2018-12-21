9e8cec6ad8e84c4d8db5adbe61960cbad10cfedd
Antonio Serrano Martin <34432449+Freethepizza@users.noreply.github.com>
2018-12-21 11:55:36 +0100

    Handler
    added handler test

--- js/draggable.js
+++ js/draggable.js
@@ -7,6 +7,7 @@ let principal = ".dotted";
 $(function () {
     $('[id^="drag-"]').draggable({
         containment: principal,
+        handle: '#handler',
     }).resizable({
         containment: principal,
         minWidth: 150,
